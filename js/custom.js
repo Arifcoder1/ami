792a9fd9c4f2d8532ef188a0ed9c8537e4bd808e
Arifcoder <freelancerarif.khan.me@gmail.com>
2022-10-15 11:01:00 +0600

    demo

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+
+
+var typed = new Typed('.hasan', {
+    strings: [
+    'freelancer', 
+    'Web Design.', 
+    'Web Development.', 
+    'Md.Arifur Rahaman.', 
+],
+    typeSpeed:200,
+    loop:true
+  });
